e8585ea5f7b56c0e3a3abce19b918e1a5bd0136f
xpan2 <xpan@walmartlabs.com>
2016-09-25 21:47:09 -0700

    Style main and example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function () {
@@ -11,8 +12,16 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to example page!</p>
+      <h1 className="text-centered">Examples</h1>
+      <p>Here are a few example locations to try out!</p>
+      <ol>
+        <li>
+          <Link to='/?location=London'>London, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   );
 };
