8867d2cfe2a72805f8ae0b4f9abe00be46e688d7
xpan2 <xpan@walmartlabs.com>
2016-09-25 20:27:34 -0700

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,10 @@ var React = require('react');
 // stateless component
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome about page!</p>
+    </div>
   );
 };
 
