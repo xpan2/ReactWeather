af193c27b595926bf47caf0d0fb5c12736fcfd59
xpan2 <xpan@walmartlabs.com>
2016-09-25 21:02:57 -0700

    Added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -9,6 +9,11 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// loading foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
