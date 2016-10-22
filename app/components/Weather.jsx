c2a0abceab0f3a3cc5159596ddeca3cfa0f0a9f0
xpan2 <xpan@walmartlabs.com>
2016-10-21 17:39:25 -0700

    New style

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,50 +1,63 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
   getInitialState: function () {
     return {
       isLoading: false
-    };
+    }
   },
-
   handleSearch: function (location) {
     var that = this;
 
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
-        isLoading: false,
         location: location,
-        temp: temp
-      })
-    }, function (errorMessage) {
-      that.setState({isLoading: false});
-      alert(errorMessage);
+        temp: temp,
+        isLoading: false
+      });
+    }, function (e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
     });
   },
-
   render: function () {
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
 
     function renderMessage () {
       if (isLoading) {
         return <h3 className="text-center">Fetching weather...</h3>;
-      } else if (temp, location) {
+      } else if (temp && location) {
         return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+
+    function renderError () {
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message={errorMessage}/>
+        )
       }
     }
 
     return (
       <div>
-        <h1 className="text-center">Get Weather</h1>
+        <h1 className="text-center page-title">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
-    );
+    )
   }
 });
 
